eb4471cbb27628f249fabb519e12c5c867dfc245
arif-rizal1122 <rizallarif32@gmail.com>
2024-12-28 14:19:58 +0800

    setup db

---
+++ src/test/java/spring_boot_api/BelajarSpringbootRestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_boot_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringbootRestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
